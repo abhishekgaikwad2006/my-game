2cb61f2edc0ba3952f8855b11b0ce66747eda023
abhishekgaikwad2006 <72813148+abhishekgaikwad2006@users.noreply.github.com>
2021-01-26 13:58:22 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,63 +1,44 @@
-var player,playerImage;
-var enemy1,enemy1Image;
-var enemy2,enemy2Image;
-var obstaclesGroup;
-var backgroundImage;
-var bullet,bulletImage;
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+
+var track, car1_img, car2_img, car3_img, car4_img;
 
 function preload(){
-playerImage=loadImage("Images/boy.png");
-enemy1Image=loadImage("Images/rob1.png");
-enemy2Image=loadImage("Images/rob2.png");
-backgroundImage=loadImage("Images/bg.jpg");
-bulletImage=loadImage("Images/bullet.png");
+  track = loadImage("../images/track.jpg");
+  car1_img = loadImage("../images/car1.png");
+  car2_img = loadImage("../images/car2.png");
+  car3_img = loadImage("../images/car3.png");
+  car4_img = loadImage("../images/car4.png");
+  ground = loadImage("../images/ground.png");
 }
 
 function setup(){
-createCanvas(1200,500);
-player=createSprite (120,350,50,50);
-player.addImage(playerImage);
-player.scale=0.5
-
-bullet=createSprite (215,312,50,50);
-bullet.addImage(bulletImage);
-bullet.scale=0.11;
-
-
-obstaclesGroup=new Group();
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
 }
+
 
 function draw(){
-background(backgroundImage);
-if(keyDown("space")){
-bullet.velocityX=5
-}
-spawnObstacles();
-drawSprites();
-}
-
-function spawnObstacles() {
-  if(frameCount % 100 === 0) {
-    var obstacle = createSprite(1000,350,10,40);
-    //obstacle.debug = true;
-    obstacle.velocityX = -6;//(6 + 3*score/100);
-    
-    //generate random obstacles
-    var rand = Math.round(random(1,2));
-    switch(rand) {
-      case 1: obstacle.addImage(enemy1Image);
-              break;
-      case 2: obstacle.addImage(enemy2Image);
-              break;
-    
-      default: break;
-    }
-    
-    //assign scale and lifetime to the obstacle           
-    obstacle.scale = 0.4;
-    obstacle.lifetime = 300;
-    //add each obstacle to the group
-    obstaclesGroup.add(obstacle);
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
   }
 }
-
