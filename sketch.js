53c781d0ccf77a0e277251a5f3933b56074766d2
abhishekgaikwad2006 <72813148+abhishekgaikwad2006@users.noreply.github.com>
2021-01-30 14:28:45 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,6 +11,11 @@ var player2,player2Image;
 var score2=0;
 var bullet2,bullet2Image,bullet2Group;
 var form;
+var player1name,player2name;
+var introBG;
+var edges;
+var gun1sound,gun2sound;
+
 function preload(){
 playerImage=loadImage("Images/boy.png");
 player2Image=loadImage("Images/player2.png");
@@ -20,72 +25,85 @@ enemy3Image=loadImage("Images/zombie3.png");
 backgroundImage=loadImage("Images/forest[71].jpg");
 bulletImage=loadImage("Images/bullet.png");
 bullet2Image=loadImage("Images/bullet2.png");
+introBG=loadImage("Images/intro_bg.jpg");
+gun1sound=loadSound("Images/gun1.mp3");
+gun2sound=loadSound("Images/gun2.mp3");
 }
 
 function setup(){
-createCanvas(1200,500);
-
-bg=createSprite (600,250,50,50);
+createCanvas(1200,700);
+edges=createEdgeSprites();
+bg=createSprite (600,350,50,50);
 bg.addImage(backgroundImage);
-bg.scale=0.7;
+bg.scale=0.9;
 bg.velocityX=-2;
 
-player=createSprite (240,340,50,50);
+player=createSprite (240,500,50,50);
 player.addImage(playerImage);
 player.scale=0.5;
 
-player2=createSprite (90,385,50,50);
+player2=createSprite (90,560,50,50);
 player2.addImage(player2Image);
 player2.scale=0.4;
 
 bulletGroup=new Group();
 bullet2Group=new Group();
 obstaclesGroup=new Group();
-
 form=new Form();
+form2=new Form2();
 }
 
 function draw(){
-background("black");
+//background("black");
 
 if(gameState===0){
+  console.log(gameState)
+  //form2.hide();
 form.display();
+
+}
+if(gameState===1){
+  console.log(gameState)
+  form2.display();
 }
 
 
-
-if (gameState=== "PLAY"){
-
+if (gameState === 2){
+  
+  spawnObstacles();
+console.log(gameState)
   if(bg.x<0){
     bg.x=600;
   }
  
-  obstaclesGroup.velocityX = -6;  //(6 + 3*score/100);
+  //obstaclesGroup.velocityX = -6;  //(6 + 3*score/100);
   
   if(keyDown("space")) {
     createBullet2();
-    bullet2.velocityX = 500;
+    gun1sound.play();
+    bullet2.velocityX = 10;
   }
   if(keyDown("enter")) {
     createBullet();
-    bullet.velocityX = 500;
+    gun2sound.play();
+    bullet.velocityX = 10;
   }
-  if(keyDown("UP_Arrow")){
-    player.y=player.y-5;
-  }
-
-  if(keyDown("DOWN_Arrow")){
-    player.y=player.y+5;
-  }
-  if(keyDown("w")){
-    player2.y=player2.y-5;
+  if(keyDown("UP_Arrow")&&player.y>500){
+    player.y=player.y-10;
   }
 
-  if(keyDown("s")){
-    player2.y=player2.y+5;
+  if(keyDown("DOWN_Arrow")&&player.y>400){
+    player.y=player.y+10;
+  }
+  if(keyDown("w")&&player2.y>500){
+    player2.y=player2.y-10;
+  }
+
+  if(keyDown("s")&&player2.y>400){
+    player2.y=player2.y+10;
   }
   
-}
+
 for(var i=0;i<obstaclesGroup.length;i++){
   if(obstaclesGroup.get(i).isTouching(bulletGroup)){
     obstaclesGroup.get(i).destroy();
@@ -101,23 +119,41 @@ for(var i=0;i<obstaclesGroup.length;i++){
  }
 }
 
-
-spawnObstacles();
-
+if(score1===10 || score2===10){
+  gameState=3;
+}
+player.collide(edges[3]);
+player2.collide(edges[3]);
 drawSprites();
 textSize(20);
 fill("white");
 textFont("Comic Sans MS");
-text("Player1 Score: "+ score1, 100,50);
-text("Player2 Score: "+ score2, 600,50);
+text(player1name +"'s Score: "+ score1, 100,50);
+text(player2name +"'s Score: "+ score2, 900,50);
 }
-
-
+if(gameState===3){
+  background("black");
+  if(score1>score2){
+    textSize(40);
+    textFont("Comic Sans MS");
+    fill("white");
+    text("Congratulations!",490,300);
+    text(player1name+" has won!",500,400);
+}
+if(score2>score1){
+  textSize(40);
+  textFont("Comic Sans MS");
+  fill("white");
+  text("Congratulations!",490,300);
+  text(player2name+" has won!",500,400);
+}
+}
+}
 function spawnObstacles() {
   if(frameCount % 250 === 0) {
-    var obstacle = createSprite(1000,350,10,40);
+    var obstacle = createSprite(1000,600,10,40);
     //obstacle.debug = true;
-    obstacle.velocityX = -8;//(6 + 3*score/100);
+    obstacle.velocityX = -5;//(6 + 3*score/100);
     
     //generate random obstacles
     var rand = Math.round(random(1,3));
@@ -126,7 +162,7 @@ function spawnObstacles() {
               break;
       case 2: obstacle.addImage(enemy2Image);
               break;
-              case 3: obstacle.addImage(enemy3Image);
+      case 3: obstacle.addImage(enemy3Image);
               break;
               
       default: break;
@@ -136,7 +172,7 @@ function spawnObstacles() {
     
     //assign scale and lifetime to the obstacle           
     obstacle.scale = 0.8;
-    obstacle.lifetime = 300;
+   // obstacle.lifetime = 300;
     //add each obstacle to the group
     obstaclesGroup.add(obstacle);
   }
@@ -158,3 +194,4 @@ function createBullet2 (){
 
   bullet2Group.add(bullet2);
 }
+
